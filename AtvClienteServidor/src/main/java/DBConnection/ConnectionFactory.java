898d45da3542dc462ff9d80fcdd6e688c79d3599
Wesley do Carmo <wesley.cangussu@outlook.com>
2022-11-06 11:28:52 -0300

    Conexão com banco de dados

---
+++ AtvClienteServidor/src/main/java/DBConnection/ConnectionFactory.java
@@ -0,0 +1,20 @@
+package DBConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+	public Connection getConnection() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			return DriverManager.getConnection("jdbc:mysql://localhost/bdlivraria?useTimezone=true&serverTimezone=UTC","root", "root");
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+}
