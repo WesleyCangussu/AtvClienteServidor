c829e689b5b6d43b62bdcfa078bd3281e255c581
Paulo Leal <devpauloleal@gmail.com>
2022-11-06 15:04:59 -0300

    Criação do CategoryDao

---
+++ AtvClienteServidor/src/main/java/dao/CategoryDao.java
@@ -0,0 +1,38 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import models.Category;
+
+import DBConnection.ConnectionFactory;
+
+public class CategoryDao {
+	private Connection connection;
+
+    public CategoryDao() {
+
+        this.connection = new ConnectionFactory().getConnection();
+    }
+
+    public List<Category> getCategories(){
+        try {
+            List<Category> categories = new ArrayList<Category>();
+            PreparedStatement stmt = this.connection.prepareStatement("select * from tbcategorias");
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                Category category = new Category();
+                category.setCategory(rs.getString("categoria"));
+                categories.add(category);
+            }
+            rs.close();
+            stmt.close();
+            return categories;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
