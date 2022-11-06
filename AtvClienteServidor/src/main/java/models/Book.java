0a7c237aa0f10ea83972a4946d6112252355945e
Vanessa Ike <vanessaike7@gmail.com>
2022-11-06 14:19:44 -0300

    criação das classes categoria e livro

---
+++ AtvClienteServidor/src/main/java/models/Book.java
@@ -0,0 +1,38 @@
+package models;
+
+public class Book {
+	 private String title, author, category;
+	    private int id;
+	    private float value;
+
+	    public String getTitle() {
+	        return title;
+	    }
+	    public void setTitle(String title) {
+	        this.title = title;
+	    }
+	    public String getAuthor() {
+	        return author;
+	    }
+	    public void setAutor(String author) {
+	        this.author = author;
+	    }
+	    public String getCategory() {
+	        return category;
+	    }
+	    public void setCategory(String category) {
+	        this.category = category;
+	    }
+	    public int getId() {
+	        return id;
+	    }
+	    public void setId(int id) {
+	        this.id = id;
+	    }
+	    public float getValue() {
+	        return value;
+	    }
+	    public void setValor(float value) {
+	        this.value = value;
+	    }
+}
---
+++ AtvClienteServidor/src/main/java/models/Category.java
@@ -0,0 +1,13 @@
+package models;
+
+public class Category {
+    private String category;
+
+    public String getCategory() {
+        return this.category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
